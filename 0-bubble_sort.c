aa7501c879259a0639e98157e541b9edded83676
AkpokigheMarvellous <m.ovigwe@yahoo.com>
2022-10-07 08:55:08 -0700

    A function that sorts an array of integers in ascending order using the Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,35 +1,33 @@
-/*
- * File: 0-bubble_sort.c
- * Auth: Akpokighe O. Marvellous
- */
 #include "sort.h"
-
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- * using the Bubble sort algorith.
+ *bubble_sort - Function that sorts an array of ints
+ *using bubble sort algorithm
  *
- * @array: Array of integers to be sorted
- * @size: The size of the @array
+ *@array: array of ints
+ *@size: size of array
+ *
  */
-
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
 	int temp;
+	size_t i, k;
 
-	for (i = 0; i < size; i++)
+	if (!array || !size)
+		return;
+
+	i = 0;
+	while (i < size)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (k = 0; k < size - 1; k++)
 		{
-			/* if this pair is out of order */
-			if (array[j] > array[j + 1])
+			if (array[k] > array[k + 1])
 			{
-				/* swap them and remember something changed */
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				temp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = temp;
 				print_array(array, size);
 			}
 		}
+		i++;
 	}
 }
--- sort.h
+++ sort.h
@@ -1,43 +1,43 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef _SORT_H
+#define _SORT_H
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 /**
  * struct listint_s - Doubly linked list node
  *
  * @n: Integer stored in the node
- *@prev: Pointer to the previous element of the list
- *@next: Pointer to the next lement of the list
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-	const int n;
+	int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
 
-/*Provided Function prototypes */
+
+/* print.c */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void check_list(const listint_t *list);
+void check_array(const int *array, size_t size);
+int *rand_array(int len, int max);
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-/* My functions prototypes*/
-void swap(int *x, int *y);
-size_t lomuto_partition(int *array, int low, int high, size_t size);
-void lomuto_scheme(int *array, int low, int high, size_t size);
-
-/* Advanced/optional tasks function prototypes*/
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
-#endif /* SORT_H */
+
+#endif

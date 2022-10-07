c836cb6ac25f59710745eac8114efcd3b8334aac
AkpokigheMarvellous <m.ovigwe@yahoo.com>
2022-10-07 09:05:55 -0700

    function that sorts a doubly linked list of integers in ascending order using the Insertion sort algorithm

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,51 +1,54 @@
-/*
- * File: 1-insertion_sort_list.c
- * Auth: Akpokighe O. Marvellous
- */
-
 #include "sort.h"
-/**
- *swapNodes - Swapt two nodes in a doubly linekd list.
- *@list: pointer to the head of the doubly linekd list.
- *@swap1: A pointer to the first node to swap.
- *@swap2: The second node to swap.
- */
 
-void swapNodes(listint_t **list, listint_t **swap1, listint_t *swap2)
+/**
+ * swap - swaps 2 nodes in a doubly-linked list
+ * @a: address of first node
+ * @b: address of second node
+ *
+ * Return: void
+ */
+void swap(listint_t *a, listint_t *b)
 {
-	(*swap1)->next = swap2->next;
-	if (swap2->next != NULL)
-		swap2->next->prev = *swap1;
-	swap2->prev = (*swap1)->prev;
-	swap2->next = *swap1;
-	if ((*swap1)->prev != NULL)
-		(*swap1)->prev->next = swap2;
-	else
-		*list = swap2;
-	(*swap1)->prev = swap2;
-	*swap1 = swap2->prev;
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+
 }
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of integers in
- * ascending order using the insertion sort algorithm.
- * @list: The list to be sorted
+ * insertion_sort_list - insertion sorts a doubly-linked list
+ * @list: address of pointer to head node
+ *
+ * Return: void
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *i, *insert, *tmp;
+	listint_t *i, *j;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
+	if (!list || !*list || !(*list)->next)
 		return;
-
-	for (i = (*list)->next; i != NULL; i = tmp)
+	i = (*list)->next;
+	while (i)
 	{
-		tmp = i->next;
-		insert = i->prev;
-		while (insert != NULL && i->n < insert->n)
+		j = i;
+		i = i->next;
+		while (j && j->prev)
 		{
-			swapNodes(list, &insert, i);
-			print_list((const listint_t *)*list);
+			if (j->prev->n > j->n)
+			{
+				swap(j->prev, j);
+				if (!j->prev)
+					*list = j;
+				print_list((const listint_t *)*list);
+			}
+			else
+				j = j->prev;
 		}
+
 	}
 }

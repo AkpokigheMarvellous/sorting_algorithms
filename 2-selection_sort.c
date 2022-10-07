868e6866d63421d6f08e853431378c4dca760580
AkpokigheMarvellous <m.ovigwe@yahoo.com>
2022-10-07 09:09:08 -0700

    A  function that sorts an array of integers in ascending order using the Selection sort algorithm

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,49 +1,32 @@
-/*
- * File: 2-selection_sort.c
- * Auth: Akpokighe O. Marvellous
- */
 #include "sort.h"
-
 /**
- * selection_sort - Sorts an array of integers in ascedning order
- *  using the selection sort algorithm.
- *@array: The array to be sorted
- *@size: The size of the array
+ *selection_sort- function that sorts using selection sort algorithm
+ *
+ *@array: array to be sorted
+ *@size: size of the array
+ *
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, jmin, jtmp;
-	int itmp;
+	size_t i, j, k;
+	int temp;
 
-	/* loop through the entire array */
+	if (!array || !size)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
-		/*
-		 * Find the smallest element in the unsorted array[i...size-1]
-		 * Assume the first element is the minimum
-		 */
-		jmin = i;
-		/* Compare against elements after i to find the smallest */
-		for (j = i + 1; j < size; j++)
+		for (j = size - 1, k = i + 1; j > i; j--)
 		{
-
-			/* if this element is smaller, then it is the new smallest */
-			if (array[j] < array[jmin])
+			if (array[j] < array[k])
 			{
-				/* found the new smallest; remember the index */
-				jmin = j;
+				k = j;
 			}
 		}
-		/* swap */
-		if (jmin != i)
+		if (array[i] > array[k])
 		{
-			itmp = array[i]; /*
-					  * Save the value at index i to be swapped
-					  * with the smallest element later
-					  */
-			jtmp = jmin; /* Save the samllest elements index before swapping */
-			array[i] = array[jmin];
-			array[jtmp] = itmp;
+			temp = array[i];
+			array[i] = array[k];
+			array[k] = temp;
 			print_array(array, size);
 		}
 	}
